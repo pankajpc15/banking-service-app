2215acbadb11e47966db39fc230387ac8b6aef56
pankaj <pankaj21003@gmail.com>
2024-08-02 18:01:43 +0530

    Resolved merge conflicts

--- banking-service/src/main/java/com/financial/banking/BankingServiceApplication.java
+++ banking-service/src/main/java/com/financial/banking/BankingServiceApplication.java
@@ -9,24 +9,18 @@ public class BankingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankingServiceApplication.class, args);
 	}
+	
+}
+
 
 	
-
+//created by pankak chaudhari
+//	https://www.linkedin.com/in/pankaj-chaudhari/
+//	https://github.com/pankajpc15
 	
 //	go to chrome broswer and paste the url 
 	
 //	http://localhost:8090/swagger-ui/index.html
 	
-}
-//
-//
-//<plugin>
-//<groupId>org.apache.maven.plugins</groupId>
-//<artifactId>maven-compiler-plugin</artifactId>
-//<version>3.11.0</version> <!-- Use the latest version if necessary -->
-//<configuration>
-//    <source>${java.version}</source>
-//    <target>${java.version}</target>
-//</configuration>
-//</plugin>
+
 
